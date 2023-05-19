dedbde062b7808956644268bba39c2c001f9ecf6
nipaaa <akternipa941@gmail.com>
2023-05-19 12:16:25 +0600

    header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import close from "../assets/close.png";
+import arrow from "../assets/down-arrow.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg ">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              <img src={logo} alt="" />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarTogglerDemo02"
+              aria-controls="navbarTogglerDemo02"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <div className="ms-auto header_buttons">
+                <div className="dropdown">
+                  <button
+                    className="btn dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Short Response{" "}
+                    <img style={{ marginLeft: "46px" }} src={arrow} alt="" />
+                  </button>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <button className="close_btn">
+                  <img src={close} alt="" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
