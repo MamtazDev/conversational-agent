b8c7a0f11f6a68787510712dc2b4f516547b9b59
nipaaa <akternipa941@gmail.com>
2023-05-20 14:51:39 +0600

    update

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -7,15 +7,21 @@ import messenger from "../assets/ai-face.png";
 import { useRef, useState } from "react";
 
 const Conversation = () => {
+  const [vassHistory, setVaasHistory] = useState([]);
+  const [newVassHistory, setNewVassHistory] = useState("");
   const [display, setDisplay] = useState(false);
   const [text, setText] = useState("");
+
+  const [vaasId, setVaasId] = useState(null);
+
   const textareaRef = useRef(null);
 
   const maxRows = 4; // Maximum number of rows allowed
   const lineHeight = 20; // Line height of the textarea
 
   const handleChange = (event) => {
-    setText(event.target.value);
+    // setText(event.target.value);
+    setNewVassHistory(event.target.value);
     adjustTextareaHeight();
   };
 
@@ -33,42 +39,85 @@ const Conversation = () => {
       }
     }
   };
+
+  const initialApi = () => {
+    fetch("https://testenv.innobyteslab.com/vaas/", {
+      headers: {
+        "VAAS-API-Key": "test-x0848bd789fjk13",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setVaasId(data.vaas_sid);
+      });
+  };
+
+  const chatHandler = () => {
+    initialApi();
+  };
+
+  const handleUpdate = () => {
+    const apiKey = "test-x0848bd789fjk13";
+    // const vaasSid = "83b04e59fc7403b2848f279fa2722c73";
+    const question = "why are you";
+
+    fetch("https://testenv.innobyteslab.com/vaas/history/", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "VAAS-API-Key": apiKey,
+        "vaas_sid": vaasId,
+        "question": question,
+      },
+      body: JSON.stringify({ data: newVassHistory }),
+    })
+      .then((response) => {
+     
+        console.log("Data updated successfully");
+        setVaasHistory(newVassHistory); 
+        setNewVassHistory(""); 
+      })
+      .catch((error) => {
+     
+        console.error("Error updating data:", error);
+      });
+  };
+
   return (
     <div className="conversation">
       <div className="container">
         {display && (
           <>
             <div className="chatting">
-              {[1, 2, 3, 4, 5, 6, 7, 5, 6, 7, 8, 9].map((chat, index) => (
-                <div key={index}>
-                  <div className="answer">
-                    <img src={aiFace} alt="" />
-                    <p>
-                      Welcome to VA Chat. I’m your personal assistant ot the
-                      company and its service. How may I help you today?
-                    </p>
-                    <div className="reaction">
-                      <img src={like} alt="" />
-                      <img src={dislike} alt="" />
+              {vassHistory.length &&
+                vassHistory.map((chat, index) => (
+                  <div key={index}>
+                    <div className="answer">
+                      <img src={aiFace} alt="" />
+                      <p>{chat.answer}</p>
+                      <div className="reaction">
+                        <img src={like} alt="" />
+                        <img src={dislike} alt="" />
+                      </div>
+                    </div>
+                    <div className="question">
+                      <p>{chat.question}</p>
+                      <img src={user} alt="" />
                     </div>
                   </div>
-                  <div className="question">
-                    <p>Do you provide me consulting services? Which ones?</p>
-                    <img src={user} alt="" />
-                  </div>
-                </div>
-              ))}
+                ))}
             </div>
             <div className="input_field">
               <div className="user_input">
                 <textarea
                   ref={textareaRef}
-                  value={text}
+                  value={newVassHistory}
                   onChange={handleChange}
                   placeholder="Type your question here.... (Scribe tu pregunta aqui....)"
                 />
 
-                <button>
+                <button onClick={handleUpdate}>
                   <img src={send} alt="" />
                 </button>
               </div>
@@ -77,7 +126,10 @@ const Conversation = () => {
         )}
 
         <img
-          onClick={() => setDisplay(!display)}
+          onClick={() => {
+            setDisplay(!display);
+            chatHandler();
+          }}
           className="sms_icon img-fluid"
           src={messenger}
           alt=""
